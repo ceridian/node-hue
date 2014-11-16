10db749075a38a085d20ddd60656fe458fc71c51
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2014-11-15 23:14:43 -0500

    test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var a = require('../lib/auth.js');
 
 router.get('/', a.checkAuth, function(req, res) {
 	console.log('following index route');
-	res.render('index', { title: 'Index'});
+	res.render('index', { user: req.session.user_id });
 });
 
 module.exports = router;
