b482ec3f28cde2c307335ce77eb9d485fc77695c
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2014-11-15 20:57:41 -0500

    testing models

---
+++ models/jobs.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+	var JOB = sequelize.define('JOB', {
+		type: { type: DataTypes.STRING, allowNull: false },
+		statement: DataTypes.STRING,
+		outDest: { type: DataTypes.STRING, allowNull: false },
+		jobID: { type: DataTypes.STRING, allowNull: false },
+		startDtTm: { type: DataTypes.DATETIME, allowNull: false },
+		endDtTm: DataTypes.DATE
+	}, {
+		classMethods: {
+			associate: function(models){
+				JOB.belongsTo(models.USER);
+			}
+		}
+	});
+	return JOB;
+};
---
+++ models/users.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+	var USER = sequelize.define('USER', {
+		user: { type: DataTypes.STRING, allowNull: false },
+		pass: { type: DataTypes.STRING, allowNull: false },
+		email: DataTypes.STRING,
+		firstName: DataTypes.STRING,
+		lastName: DataTypes.STRING,
+	}, {
+		classMethods: {
+			associate: function(models){
+				USER.hasMany(models.JOB, {as: 'user'});
+			}
+		}
+	});
+	return USER;
+};
