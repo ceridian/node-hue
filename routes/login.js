078e88e277ddea5f657c580ef69d505d4582fcf6
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2014-11-15 22:05:48 -0500

    auth

--- routes/login.js
+++ routes/login.js
@@ -14,13 +14,21 @@ router.post('/', function(req, res){
 	var pass = body.pass;
 	console.log('user: '+user);
 	console.log('pass: '+pass);
-	models.USER.findAll().complete(function(err, data){
+	models.USER.find({where: { user: user}}).complete(function(err, data){
 		if(err){
 			console.log(err);
 			res.send(err);
 		}else{
-			console.log(data);
-			res.send('testting');
+			if(data.length == 0){
+				res.send('Unknown User');
+			}else{
+				var u = data[0];
+				if(u != pass){
+					res.send('Incorect Password');
+				}else{
+					res.send('ok');
+				}
+			}
 		}
 	});
 });
