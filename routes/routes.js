ddc5253d3eb9484960664fb835d70bd3b5d57040
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-05-20 19:58:11 -0400

    test

--- routes/routes.js
+++ routes/routes.js
@@ -169,7 +169,7 @@ module.exports = function(app) {
     });
   });
 
-  app.get('/dir', a.checkAuth, function(req, res) {
+  app.post('/dir', a.checkAuth, function(req, res) {
     var body = req.body;
     console.log(body);
     var dir = body.dir;
