c83161a20439e8fdaf02a416df4595435b771b4d
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-05-21 15:05:08 -0400

    test

--- public/js/lib/app.js
+++ public/js/lib/app.js
@@ -61,48 +61,6 @@
 		});
 	});
 
-	app.factory('RecursionHelper', ['$compile', function($compile){
-		return {
-			/**
-			 * Manually compiles the element, fixing the recursion loop.
-			 * @param element
-			 * @param [link] A post-link function, or an object with function(s) registered via pre and post properties.
-			 * @returns An object containing the linking functions.
-			 */
-			compile: function(element, link){
-				// Normalize the link parameter
-				if(angular.isFunction(link)){
-					link = { post: link };
-				}
-
-				// Break the recursion loop by removing the contents
-				var contents = element.contents().remove();
-				var compiledContents;
-				return {
-					pre: (link && link.pre) ? link.pre : null,
-					/**
-					 * Compiles and re-adds the contents
-					 */
-					post: function(scope, element){
-						// Compile the contents
-						if(!compiledContents){
-							compiledContents = $compile(contents);
-						}
-						// Re-add the compiled contents to the element
-						compiledContents(scope, function(clone){
-							element.append(clone);
-						});
-
-						// Call the post-linking function, if any
-						if(link && link.post){
-							link.post.apply(null, arguments);
-						}
-					}
-				};
-			}
-		};
-	}]);
-
 	app.factory('socket', ['$rootScope', function ($rootScope) {
 		var socket = io.connect();
 		return {
@@ -132,11 +90,6 @@
 			restrict: "E",
 			scope: {treeData: '=treeData'},
 			templateUrl: "temps/hdfs.html",
-			/*compile: function(element){
-				return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
-
-				});
-			}*/
 		}
 	});
 
